0a065b89480c7748e13fe60e3fcaf2398124c5a9
simileas <simileas@gmail.com>
2020-04-23 15:44:57 +0800

    fix: 修改 .kts 文件格式

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -1,39 +1,39 @@
 import com.moowork.gradle.node.npm.NpmTask
 
 plugins {
-    java
-    id("com.moowork.node") version ("1.3.1")
+  java
+  id("com.moowork.node") version ("1.3.1")
 }
 
 apply(plugin = "com.moowork.node")
 
 tasks.named<Jar>("jar") {
-    enabled = false
+  enabled = false
 }
 
 node() {
-    version = "10.16.0"
-    npmVersion = "6.4.1"
-    distBaseUrl = "https://npm.taobao.org/mirrors/node/"
-    download = true
-    workDir = file("${project.buildDir}/node")
-    npmWorkDir = file("${project.buildDir}/npm")
-    nodeModulesDir = file("${project.projectDir}")
+  version = "10.16.0"
+  npmVersion = "6.4.1"
+  distBaseUrl = "https://npm.taobao.org/mirrors/node/"
+  download = true
+  workDir = file("${project.buildDir}/node")
+  npmWorkDir = file("${project.buildDir}/npm")
+  nodeModulesDir = file("${project.projectDir}")
 }
 
 tasks.register<NpmTask>("npmInstallVerbose") {
-    setArgs(listOf("install", "-verbose"))
+  setArgs(listOf("install", "-verbose"))
 }
 
 tasks.register<NpmTask>("bundle") {
-    dependsOn("npmInstallVerbose")
-    setArgs(listOf("run", "build"))
+  dependsOn("npmInstallVerbose")
+  setArgs(listOf("run", "build"))
 }
 
 tasks.named<DefaultTask>("build") {
-    dependsOn("bundle")
+  dependsOn("bundle")
 }
 
 tasks.register<NpmTask>("npmStartVerbose") {
-    setArgs(listOf("start", "-verbose"))
+  setArgs(listOf("start", "-verbose"))
 }
